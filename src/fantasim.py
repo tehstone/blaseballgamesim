efefd5a554c3ff268092b0ae8ac94d18e68b0fd9
tehstone <stone.johnny@gmail.com>
2021-03-23 23:18:59 -0700

    fix fantasy sim so it handles the same team playing itself

--- src/fantasim.py
+++ src/fantasim.py
@@ -2,7 +2,7 @@ import json
 import os
 from typing import Dict, Any
 
-from src.common import blood_id_map, fk_key, blood_name_map, Team, team_id_map, Weather, get_player_stlats
+from src.common import blood_id_map, fk_key, blood_name_map, Team, team_id_map, Weather, get_player_stlats, BloodType
 from src.game_state import GameState, InningHalf
 from src.team_state import DEF_ID, TeamState
 from src.common import ForbiddenKnowledge as FK
@@ -18,7 +18,15 @@ def get_stlat_dict(player: Dict[str, Any]) -> Dict[FK, float]:
     return ret_val
 
 
-def load_state(team_info, team_id, season, day):
+def load_state(team_id, season, day):
+    team_info = {
+        "lineup": {},
+        "rotation": {},
+        "blood": {},
+        "game_stats": {},
+        "stlats": {},
+        "names": {}
+    }
     player_stlats_list = get_player_stlats(season, day)
     for player in player_stlats_list:
         if day == 6 and player["team_id"] == "105bc3ff-1320-4e37-8ef0-8d595cb95dd0":
@@ -29,60 +37,41 @@ def load_state(team_info, team_id, season, day):
         pos = int(player["position_id"]) + 1
         if "position_type_id" in player:
             if player["position_type_id"] == "0":
-                team_info[team_id]["lineup"][pos] = player_id
+                team_info["lineup"][pos] = player_id
             else:
-                team_info[team_id]["rotation"][pos] = player_id
+                team_info["rotation"][pos] = player_id
         else:
             if player["position_type"] == "BATTER":
-                team_info[team_id]["lineup"][pos] = player_id
+                team_info["lineup"][pos] = player_id
             else:
-                team_info[team_id]["rotation"][pos] = player_id
-        team_info[team_id]["stlats"][player_id] = get_stlat_dict(player)
+                team_info["rotation"][pos] = player_id
+        team_info["stlats"][player_id] = get_stlat_dict(player)
 
-        team_info[team_id]["game_stats"][player_id] = {}
-        team_info[team_id]["game_stats"][DEF_ID] = {}
+        team_info["game_stats"][player_id] = {}
+        team_info["game_stats"][DEF_ID] = {}
 
-        team_info[team_id]["names"][player_id] = player["player_name"]
+        team_info["names"][player_id] = player["player_name"]
 
         try:
-            team_info[team_id]["blood"][player_id] = blood_id_map[int(player["blood"])]
+            team_info["blood"][player_id] = blood_id_map[int(player["blood"])]
         except ValueError:
-            team_info[team_id]["blood"][player_id] = blood_name_map[player["blood"]]
+            team_info["blood"][player_id] = blood_name_map[player["blood"]]
+        except TypeError:
+            team_info["blood"][player_id] = BloodType.A
     return team_info
 
-# s7 d1 crabs
-home_team = "8d87c468-699a-47a8-b40d-cfb73a5660ad"
-h_team_pitcher = "3af96a6b-866c-4b03-bc14-090acf6ecee5"
-#s14 d99 tigers
-away_team = "747b8e4a-7e50-4638-a973-ea7950a3e739"
-a_team_pitcher = "2720559e-9173-4042-aaa0-d3852b72ab2e"
-h_team_season = 6
+# s3 d1 wings
+home_team = "57ec08cc-0411-4643-b304-0e80dbc15ac7"
+h_team_pitcher = "089af518-e27c-4256-adc8-62e3f4b30f43"
+#s14 d99 wings
+away_team = "57ec08cc-0411-4643-b304-0e80dbc15ac7"
+a_team_pitcher = "65273615-22d5-4df1-9a73-707b23e828d5"
+h_team_season = 2
 a_team_season = 13
 h_team_day = 0
 a_team_day = 98
 
-team_info = {
-    home_team: {
-        "lineup": {},
-        "rotation": {},
-        "blood": {},
-        "game_stats": {},
-        "stlats": {},
-        "names": {}
-    },
-    away_team: {
-        "lineup": {},
-        "rotation": {},
-        "blood": {},
-        "game_stats": {},
-        "stlats": {},
-        "names": {}
-    }
-}
-
-load_state(team_info, home_team, h_team_season, h_team_day)
-load_state(team_info, away_team, a_team_season, a_team_day)
-
+team_info = load_state(home_team, h_team_season, h_team_day)
 home_team_state = TeamState(
             team_id=home_team,
             season=h_team_season,
@@ -93,16 +82,17 @@ home_team_state = TeamState(
             balls_for_walk=4,
             strikes_for_out=3,
             outs_for_inning=3,
-            lineup=team_info[home_team]["lineup"],
-            rotation=team_info[home_team]["rotation"],
+            lineup=team_info["lineup"],
+            rotation=team_info["rotation"],
             starting_pitcher=h_team_pitcher,
-            stlats=team_info[home_team]["stlats"],
-            game_stats=team_info[home_team]["game_stats"],
-            blood=team_info[home_team]["blood"],
-            player_names=team_info[home_team]["names"],
+            stlats=team_info["stlats"],
+            game_stats=team_info["game_stats"],
+            blood=team_info["blood"],
+            player_names=team_info["names"],
             cur_batter_pos=1,
         )
 
+team_info = load_state(away_team, a_team_season, a_team_day)
 away_team_state = TeamState(
             team_id=away_team,
             season=a_team_season,
@@ -113,13 +103,13 @@ away_team_state = TeamState(
             balls_for_walk=4,
             strikes_for_out=3,
             outs_for_inning=3,
-            lineup=team_info[away_team]["lineup"],
-            rotation=team_info[away_team]["rotation"],
+            lineup=team_info["lineup"],
+            rotation=team_info["rotation"],
             starting_pitcher=a_team_pitcher,
-            stlats=team_info[away_team]["stlats"],
-            game_stats=team_info[away_team]["game_stats"],
-            blood=team_info[away_team]["blood"],
-            player_names=team_info[away_team]["names"],
+            stlats=team_info["stlats"],
+            game_stats=team_info["game_stats"],
+            blood=team_info["blood"],
+            player_names=team_info["names"],
             cur_batter_pos=1,
         )
 
